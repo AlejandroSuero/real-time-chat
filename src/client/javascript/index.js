30befdb8b6b4507844484eef54bbb6b21c117847
AlejandroSuero <asuerome@gmail.com>
2023-10-20 22:20:06 +0200

    Added turso DB, display messages with random username

--- src/client/javascript/index.js
+++ src/client/javascript/index.js
@@ -1,17 +1,42 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { io } from 'https://cdn.socket.io/4.7.2/socket.io.esm.min.js'
 
-const socket = io()
+const getUsername = async () => {
+  const username = localStorage.getItem('username')
+  console.log('username', username)
+  if (username !== null) return username
+  const res = await fetch('https://random-data-api.com/api/users/random_user')
+  const { username: randomUsername } = await res.json()
+  localStorage.setItem('username', randomUsername)
+  return randomUsername
+}
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const socket = io({
+  auth: {
+    username: await getUsername(),
+    serverOffset: 0
+  }
+})
+
 const $ = (selectors) => document.querySelector(selectors)
 
 const $form = $('#form')
 const $input = $('#input')
+const $messages = $('#messages')
+
+socket.on('chat message', (message, serverOffset, username) => {
+  const item = `<li>
+    <p>${message}</p>
+    <small>${username}</small>
+  </li>`
+  $messages.insertAdjacentHTML('beforeend', item)
+  socket.auth.serverOffset = serverOffset
+})
 
 $form.addEventListener('submit', (e) => {
   e.preventDefault()
   if ($input.value !== '' || $input.value !== undefined) {
-    socket.emit('chat message', $input.value)
+    socket.emit('chat message', $input.value, socket.auth.username)
     $input.value = ''
   }
 })
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,17 +2,72 @@ import express from 'express'
 import logger from 'morgan'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { type ResultSet, createClient } from '@libsql/client'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
+const db = createClient({
+  url: process.env.DB_URL ?? 'libsql://db-name-username.turso.io',
+  authToken: process.env.DB_TOKEN
+})
 
-io.on('connection', (socket) => {
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    username VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`)
+
+io.on('connection', async (socket) => {
   console.log(`a user has CONNECTED! ${new Date().toLocaleDateString('es')}`)
   socket.on('disconnect', () => {
     console.log(`a user has DISCONNECTED! ${new Date().toLocaleDateString('es')}`)
   })
+  socket.on('chat message', async (message) => {
+    let result: ResultSet
+    const username: string = socket.handshake.auth.username ?? 'anonymous'
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, username) VALUES (:message, :username)',
+        args: { message, username }
+      })
+    } catch (err) {
+      if (process.env.NODE_ENV === 'production') {
+        throw new Error('Error while inserting new data into DB')
+      } else {
+        console.error(err)
+      }
+      return
+    }
+    io.emit('chat message', message, result.lastInsertRowid?.toString(), username)
+  })
+  if (!socket.recovered) {
+    try {
+      const serverOffset: number = socket.handshake.auth.serverOffset
+      const results: ResultSet = await db.execute({
+        sql: 'SELECT id, content, username FROM messages WHERE id > ?',
+        args: [serverOffset]
+      })
+      for (let i = 0; i < results.rows.length; ++i) {
+        socket.emit('chat message', results.rows[i].content, results.rows[i].id?.toString(), results.rows[i].username)
+      }
+    } catch (err) {
+      if (process.env.NODE_ENV === 'production') {
+        throw new Error('Error while retrieving data from DB')
+      } else {
+        console.error(err)
+      }
+    }
+  }
 })
 
 app.use(logger('dev'))
